9004ecc1dc76d6818a78130da9f94510af0c613d
Barbara Tess <barbaratessn@gmail.com>
2024-04-08 11:21:56 +0300

    Assignment complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams_list):
+        valid_anagrams = []
+        for anagram in anagrams_list:
+            if sorted([letter for letter in anagram]) == sorted([letter for letter in self.word]):
+                valid_anagrams.append(anagram)
+
+        return valid_anagrams
